ea09554e0b08ad363d9003b636c46e1097644732
Gal C <galc@wix.com>
2025-06-03 21:22:50 +0300

    added runi design system

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import {View, Text, Button, Colors, Spacings, Typography} from 'react-native-ui-lib';
+
+Colors.loadColors({
+  primaryColor: '#2364AA',
+  secondaryColor: '#81C3D7',
+  textColor: '##221D23',
+  errorColor: '#E63B2E',
+  successColor: '#ADC76F',
+  warnColor: '#FF963C'
+});
+
+Typography.loadTypographies({
+  heading: { fontSize: 36, fontWeight: '600' },
+  subheading: { fontSize: 28, fontWeight: '500' },
+  body: { fontSize: 18, fontWeight: '400' },
+});
+
+Spacings.loadSpacings({
+  page: 20,
+  card: 12,
+  gridGutter: 16
+});
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button label="Click me" onPress={() => alert('Button pressed')} />
     </View>
   );
 }
