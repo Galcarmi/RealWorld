079acc89ac210bfbe86f8ffe457a97f22c38cd59
Gal C <galc@wix.com>
2025-06-08 12:55:41 +0300

    added hello world home screen

---
+++ src/screens/homeScreen/homeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native-ui-lib';
+
+export const HomeScreen: React.FC = () => {
+  return (
+    <View flex center>
+      <Text text50 bold>Hello World</Text>
+    </View>
+  );
+};
