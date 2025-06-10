640fb48c1d1b2002afda8b2bf11c1c415d547ef8
Gal C <galc@wix.com>
2025-06-10 18:37:36 +0300

    fix forward ref mobs issue

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { observer } from 'mobx-react';
+import { useEffect } from 'react';
 
 import { navio } from './src/navigation/navio';
 import { userStore } from './src/store/userStore';
@@ -11,14 +12,18 @@ SplashScreen.preventAutoHideAsync();
 const AppWrapper = observer(() => {
   const isThemeReady = useAppTheme();
 
+  useEffect(() => {
+    if (isThemeReady) {
+      if (userStore.isAuthenticated()) {
+        navio.setRoot('tabs', 'MainTabs');
+      } else {
+        navio.setRoot('tabs', 'AuthTabs');
+      }
+    }
+  }, [isThemeReady, userStore.isAuthenticated()]);
+
   if (!isThemeReady) {
     return null;
-  }
-
-  if (userStore.isAuthenticated()) {
-    navio.setRoot('tabs', 'MainTabs');
-  } else {
-    navio.setRoot('tabs', 'AuthTabs');
   }
 
   return (
