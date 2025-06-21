203a2faed3a87347ad43c86d3eb4eb43045b410d
Gal C <galc@wix.com>
2025-06-21 15:21:47 +0300

    wip

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,13 +1,15 @@
 import './commonMocks';
 
-export { getMockNavigationService, resetAllServiceMocks } from './services';
+export { getMockNavigationService } from './services';
 export {
   getMockAuthStore,
   getMockUserStore,
   getMockArticlesStore,
-  resetAllStoreMocks,
 } from './stores';
 export { mockUser, mockUserMinimal, mockArticle, mockArticles } from './data';
-export { mockUtilities, resetUtilityMocks } from './utilities';
-export { getMockNavigation, resetAllNavigationMocks } from './navigation';
-export { getMockUseAuthorProfile, resetAllHookMocks } from './hooks';
+export { getMockUseAuthorProfile } from './hooks';
+export {
+  setMockRoute,
+  mockAuthorProfileRoute,
+  setMockRouteParams,
+} from './navigation';
